e540977678be7e0fcf8bb5fc3901ee95095648ef
Subham Sengupta <subham@Subhams-MacBook-Pro.local>
2023-06-23 13:21:45 +0530

    first commit

--- src/registry.ts
+++ src/registry.ts
@@ -1,67 +1,50 @@
 import { BigInt } from "@graphprotocol/graph-ts"
+
 import {
-  Registry,
-  Lend,
-  Rent,
-  RentClaimed,
-  StopLend,
-  StopRent
+  Lend as LendEvent,
+  Rent as RentEvent,
 } from "../generated/Registry/Registry"
-import { ExampleEntity } from "../generated/schema"
+import { Lend, Rent } from "../generated/schema"
 
-export function handleLend(event: Lend): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleLend(event: LendEvent): void {
+  let id =
+    event.params.nftAddress.toHex() + "-" + event.params.tokenID.toString()
+  let lend = Lend.load(id)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (!lend) {
+    let lend = new Lend(id)
+    lend.nftAddress = event.params.nftAddress
+    lend.tokenID = event.params.tokenID
+    lend.is721 = event.params.is721
+    lend.lenderAddress = event.params.lenderAddress
+    lend.lendingID = event.params.lendingID
+    lend.maxRentDuration = BigInt.fromI32(event.params.maxRentDuration)
+    lend.dailyRentPrice = BigInt.fromI32(event.params.dailyRentPrice.toI32())
+    lend.lendAmount = BigInt.fromI32(event.params.lendAmount)
+    lend.paymentToken = BigInt.fromI32(event.params.paymentToken)
+    lend.willAutoRenew = event.params.willAutoRenew
+    lend.save()
+  } else {
+    lend.is721 = event.params.is721
+    lend.lenderAddress = event.params.lenderAddress
+    lend.lendingID = event.params.lendingID
+    lend.maxRentDuration = BigInt.fromI32(event.params.maxRentDuration)
+    lend.dailyRentPrice = BigInt.fromI32(event.params.dailyRentPrice.toI32())
+    lend.lendAmount = BigInt.fromI32(event.params.lendAmount)
+    lend.paymentToken = BigInt.fromI32(event.params.paymentToken)
+    lend.willAutoRenew = event.params.willAutoRenew
+    lend.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.is721 = event.params.is721
-  entity.lenderAddress = event.params.lenderAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getLending(...)
-  // - contract.getRenting(...)
-  // - contract.onERC1155BatchReceived(...)
-  // - contract.onERC1155Received(...)
-  // - contract.onERC721Received(...)
-  // - contract.paused(...)
-  // - contract.rentFee(...)
-  // - contract.supportsInterface(...)
 }
 
-export function handleRent(event: Rent): void {}
-
-export function handleRentClaimed(event: RentClaimed): void {}
-
-export function handleStopLend(event: StopLend): void {}
-
-export function handleStopRent(event: StopRent): void {}
+export function handleRent(event: RentEvent): void {
+  let id = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let rent = new Rent(id)
+  rent.renterAddress = event.params.renterAddress
+  rent.lendingID = event.params.lendingID
+  rent.rentingID = event.params.rentingID
+  rent.rentAmount = BigInt.fromI32(event.params.rentAmount)
+  rent.rentDuration = BigInt.fromI32(event.params.rentDuration)
+  rent.rentedAt = event.params.rentedAt
+  rent.save()
+}
